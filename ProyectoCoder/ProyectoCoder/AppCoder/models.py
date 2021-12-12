edab8bf042e7c6bf05804c6579cea4aaba41157f
Jorge Alexander <jlalexander91@hotmail.com>
2021-12-12 10:40:40 -0600

    Version 3. Hasta este punto se llego en la calse 19

--- ProyectoCoder/ProyectoCoder/AppCoder/models.py
+++ ProyectoCoder/ProyectoCoder/AppCoder/models.py
@@ -6,3 +6,11 @@ class Curso(models.Model):
 
     nombre = models.CharField(max_length=40)
     camada = models.IntegerField()
+
+class Jugador(models.Model):
+
+    apellido = models.CharField(max_length=40)
+    numero = models.IntegerField()
+    esBueno = models.BooleanField()
+
+
--- ProyectoCoder/ProyectoCoder/ProyectoCoder/settings.py
+++ ProyectoCoder/ProyectoCoder/ProyectoCoder/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'ProyectoCoder.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["C:/Users/jlale/OneDrive/Documentos/ejemplosDjango23850/ProyectoCoder/ProyectoCoder/AppCoder/template"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
